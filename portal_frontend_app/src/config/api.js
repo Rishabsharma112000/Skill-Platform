6ac4639b092b7e576e69c29c70308ba5794dad0e
Rishab Sharma <rishabsharma112000@gmail.com>
2025-11-18 00:09:20 +0530

    updated api url

--- portal_frontend_app/src/config/api.js
+++ portal_frontend_app/src/config/api.js
@@ -1,5 +1,5 @@
 
-const API_BASE_URL = process.env.API_BASE_URL || "http://localhost:4000";
+const API_BASE_URL = "https://skill-platform-production-808e.up.railway.app"
 
 const API_ROUTES = {
   AUTH: {
