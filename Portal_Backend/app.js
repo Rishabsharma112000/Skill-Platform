daaa10173f2c77b1787f4066553d48678427aa7d
Rishab Sharma <rishabsharma112000@gmail.com>
2025-11-18 01:40:20 +0530

    updated cors.

--- Portal_Backend/app.js
+++ Portal_Backend/app.js
@@ -9,7 +9,11 @@ const routes = require('./routes');
 const { initRedis } = require('./utils/redis');
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 app.use(morgan('dev'));
 
